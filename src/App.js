0f66c9eb3daaf20bb3f864bc5b46d8d458bb876b
Pa1mekala37 <pavanmekala64@gmail.com>
2023-01-19 17:35:04 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
-const App = () => <h1>Remove this heading and write your code here</h1>
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
 
 export default App
